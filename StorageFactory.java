4dcf5ba0a33214373ed93f5f42388267a35b84b6
Sanju Preetham <59658774+82sanju@users.noreply.github.com>
2025-11-07 16:15:52 +0530

    Add MongoDB support and update create method
    Updated the create method to include a MongoDB connection string and added a placeholder for MySQL implementation.

--- StorageFactory.java
+++ StorageFactory.java
@@ -10,14 +10,23 @@ public class StorageFactory {
                 String user = "postgres"; // change if needed
                 String password = "password"; // change if needed
                 return new PostgreSQLTodoStorage(url, user, password);
-           /* case MYSQL:
-// Provide an example JDBC url for MySQL; user must change credentials
-                String mysqlUrl = "jdbc:mysql://localhost:3306/todo_db";
-                return new MySQLTodoStorage(mysqlUrl, "root", "password");
+            /*case MYSQL:
+                // Provide an example JDBC url for MySQL; user must change credentials
+                System.out.println("not implemented yet");
+                //String mysqlUrl = "jdbc:mysql://localhost:3306/todo_db";
+
+                //return new MySQLTodoStorage(mysqlUrl, "root", "password");
+
+
+
             case MONGODB:
-// Example: mongodb://localhost:27017
+                // Example: mongodb://localhost:27017
                 return new MongoDBTodoStorage("mongodb://localhost:27017", "todo_db");
             */
+            case MONGODB:
+                String mongoUri = "mongodb+srv://sspreetham35_db_user:<password>@cluster0.da2wynx.mongodb.net/?appName=Cluster0";
+                return new MongoDBTodoStorage(mongoUri, "todo_db");
+
             default:
                 throw new IllegalArgumentException("Unknown storage type");
         }
