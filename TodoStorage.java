431d7b208f0afb950f931cbb3f5afa0c562c35f1
Sanju Preetham <59658774+82sanju@users.noreply.github.com>
2025-11-07 10:18:42 +0530

    Update TodoStorage.java

--- TodoStorage.java
+++ TodoStorage.java
@@ -3,7 +3,7 @@ import java.util.List;
 public interface TodoStorage {
     void save(Todo todo) throws Exception;
 
-    // IDs are now integers, not strings
+    // IDs is integer
     Todo retrieve(int id) throws Exception;
 
     List<Todo> retrieveAll() throws Exception;
@@ -12,3 +12,4 @@ public interface TodoStorage {
 
     void delete(int id) throws Exception;
 }
+
